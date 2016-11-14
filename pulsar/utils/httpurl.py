da5b846af1ee5c029af9d7b365d58a366d1acf2c
Stanislav Sokolko <s.sokolko@gmail.com>
2016-11-14 17:00:21 +0500

    Fix: attempt #4

--- pulsar/utils/httpurl.py
+++ pulsar/utils/httpurl.py
@@ -1019,7 +1019,7 @@ class HttpParser:
         return len(rest)
 
     def _parse_body(self):
-        if not self._clen_rest:  # return "Already parsed" for empty response
+        if not self._clen_rest and has_empty_content(self._status_code, self._method):  # return "Already parsed" for empty response
             return 0
 
         data = b''.join(self._buf)
